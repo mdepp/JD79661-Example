08b2c344284b7a3937c2f7c1ea12aa8c24a4e9d7
rain <31737634+mdepp@users.noreply.github.com>
2025-11-24 23:29:17 -0800

    Integrate with embedded-graphics
    Adds a JD79661Display to bridge the gap between the JD79661
    driver and embedded-graphics.

---
+++ src/jd79661_display.rs
@@ -0,0 +1,97 @@
+use embedded_graphics::{pixelcolor::raw::RawU2, prelude::*, primitives::Rectangle};
+
+use crate::jd79661::{HEIGHT, PIXDEPTH, WIDTH};
+
+/// embedded_graphics support for the JD79661
+
+#[derive(PartialEq, Clone, Copy)]
+#[repr(u8)]
+#[derive(Default)]
+pub enum JD79661Color {
+    Black = 0b00,
+    #[default]
+    White = 0b01,
+    Yellow = 0b10,
+    Red = 0b11,
+}
+
+impl PixelColor for JD79661Color {
+    type Raw = RawU2;
+}
+
+const BUFFER_LENGTH: usize = WIDTH * HEIGHT * PIXDEPTH / 8;
+
+pub struct JD79661Display {
+    buffer: [u8; BUFFER_LENGTH],
+}
+
+impl JD79661Display {
+    pub fn buffer(&self) -> &[u8; BUFFER_LENGTH] {
+        &self.buffer
+    }
+}
+
+impl Default for JD79661Display {
+    fn default() -> Self {
+        let color = JD79661Color::default() as u8;
+        let byte = color | (color << 2) | (color << 4) | (color << 6);
+        Self {
+            buffer: [byte; BUFFER_LENGTH],
+        }
+    }
+}
+
+impl Dimensions for JD79661Display {
+    fn bounding_box(&self) -> Rectangle {
+        Rectangle {
+            top_left: Point { x: 0, y: 0 },
+            size: Size {
+                width: WIDTH as u32,
+                height: HEIGHT as u32,
+            },
+        }
+    }
+}
+
+impl DrawTarget for JD79661Display {
+    type Color = JD79661Color;
+
+    type Error = core::convert::Infallible;
+
+    fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
+    where
+        I: IntoIterator<Item = Pixel<Self::Color>>,
+    {
+        for pixel in pixels {
+            let Pixel(point, color) = pixel;
+
+            // XXX There is normally a 6px margin to the right which is not
+            // shown. Unsure why.
+            let point = Point {
+                x: point.x - 3,
+                y: point.y,
+            };
+
+            if !self.bounding_box().contains(point) {
+                continue;
+            }
+
+            let x = point.x as usize;
+            let y = point.y as usize;
+
+            let byte_index = y * WIDTH * PIXDEPTH / 8 + x * PIXDEPTH / 8;
+            let mut byte = self.buffer[byte_index];
+
+            let pixel_index = (x % 4) as u8;
+
+            let mask = 0b11 << (8 - (pixel_index + 1) * 2);
+            let shifted = (color as u8) << (8 - (pixel_index + 1) * 2);
+
+            byte |= shifted;
+            byte &= !mask | shifted;
+
+            self.buffer[byte_index] = byte;
+        }
+        Ok(())
+    }
+}
